969ec1ee2d1f09679b17fd6a4f1199021558644c
Knowledgeless <nazmulhossain914@gmail.com>
2020-03-02 14:47:28 +0600

    Added .sh file
    It's very boring task to type modules name one bye one. As a beginner I tried to do that using a simple script. Further automation task will be upgraded soon.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash/
+
+
+#installing pip
+sudo apt install python3-pip
+sudo apt install python-pip
+
+#installing modules
+pip3 install bs4 -y
+pip3 install requests -y
+pip3 install tqdm -y
+
+pip install bs4 -y
+pip install requests -y
+pip install tqdm -y
+
+#update & upgrade
+
+sudo apt update -y
+sudo apt upgrade -y
